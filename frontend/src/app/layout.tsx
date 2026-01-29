4f0742049a3dd005793d53391edfb2a8de9cd63e
Hanan-Nuru <hanannuru65@gmail.com>
2026-01-29 16:12:52 +0300

    lets try it

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,14 @@
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        
+        <main>{children}</main>
+      </body>
+    </html>
+  )
+}
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="p-10">
-      <h1 className="text-3xl font-bold text-center text-blue-600">Mens Collection Frontend Ready</h1>
-    </main>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
